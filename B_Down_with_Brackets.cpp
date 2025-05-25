8f8e7af6361388c6577cae066fd92b2d5a3e0972
Ashutosh Jha <ashutosh06066@gmail.com>
2025-05-25 12:28:37 +0530

    25-05

--- B_Down_with_Brackets.cpp
+++ B_Down_with_Brackets.cpp
@@ -1,33 +1,3 @@
-// Competitive Template | By: Ashutosh Jha
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <cmath>
-#include <cassert>
-
-using namespace std;
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-
-const int INF = 1e9 + 5;
-const ll LINF = 1e18;
-const int MOD = 1e9 + 7;
-
-// Macros
-#define all(v) (v).begin(), (v).end()
-#define pb push_back
-#define fi first
-#define se second
-
-// Fast IO (no sync with C stdio, untie cin/cout)
-void fast_io()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
 #include <iostream>
 #include <string>
 using namespace std;
@@ -42,48 +12,28 @@ int main() {
     while (t--) {
         string s;
         cin >> s;
+        
         int n = s.length();
         
-        // The key insight: we cannot break the sequence if and only if
-        // the sequence is of the form (^k )^k for some k >= 1
-        // i.e., k opening brackets followed by k closing brackets
         
-        bool can_break = false;
+        bool canMakeUnbalanced = false;
         
-        // Check if string is of form (((()))
-        int open_count = 0;
-        bool all_opens_first = true;
-        
-        for (int i = 0; i < n; i++) {
+
+        int balance = 0;
+        for (int i = 0; i < n - 1; i++) {
             if (s[i] == '(') {
-                open_count++;
+                balance++;
             } else {
-                all_opens_first = false;
+                balance--;
+            }
+            
+            if (balance == 0) {
+                canMakeUnbalanced = true;
                 break;
             }
         }
         
-        // If we found all opens first, check if rest are all closes
-        if (all_opens_first && open_count == n/2) {
-            // Check if remaining are all closes
-            bool all_closes_after = true;
-            for (int i = open_count; i < n; i++) {
-                if (s[i] != ')') {
-                    all_closes_after = false;
-                    break;
-                }
-            }
-            
-            if (all_closes_after) {
-                can_break = false; // Cannot break this form
-            } else {
-                can_break = true;
-            }
-        } else {
-            can_break = true; // Can break if not purely nested
-        }
-        
-        cout << (can_break ? "YES" : "NO") << "\n";
+        cout << (canMakeUnbalanced ? "YES" : "NO") << "\n";
     }
     
     return 0;
