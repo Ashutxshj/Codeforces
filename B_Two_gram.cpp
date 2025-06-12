2a9fe5f0aca39d28f7b76d37fb1ff638585be2e7
Ashutosh Jha <ashutosh06066@gmail.com>
2025-06-12 06:48:47 +0530

    12-06

--- B_Two_gram.cpp
+++ B_Two_gram.cpp
@@ -13,7 +13,6 @@ int main()
     for (int i = 0; i < s.length() - 1; i++)
     {
         string sub = s.substr(i, 2);
-        //reverse(sub.begin(), sub.end());
         mp[sub]++;
     }
     int a = 0;
