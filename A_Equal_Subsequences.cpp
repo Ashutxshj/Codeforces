04b5d5b6e94de4931ab960ba27e4524963f75ba3
Ashutosh Jha <ashutosh06066@gmail.com>
2025-06-13 13:40:18 +0530

    12-06

---
+++ A_Equal_Subsequences.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        
+        string result = string(k, '1') + string(n - k, '0');
+        
+        cout << result << endl;
+    }
+    
+    return 0;
+}
