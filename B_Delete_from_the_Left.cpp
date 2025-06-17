e3b2282d10a4f6a38eb0646e257197dc07e2c313
Ashutosh Jha <ashutosh06066@gmail.com>
2025-06-17 14:48:57 +0530

    17-06

---
+++ B_Delete_from_the_Left.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#define ll long long int
+using namespace std;
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    ll w = 0;
+    while (true)
+    {
+        int i = s.length() - w - 1;
+        int j = t.length() - w - 1;
+        if (i >= 0 && j >= 0 && s[i] == t[j])
+            w++;
+        else
+            break;
+    }
+    cout << s.length() + t.length() - 2 * w << endl;
+}
