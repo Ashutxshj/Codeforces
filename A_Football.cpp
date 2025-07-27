4c0897ca572b010ab616a675f4d75ab03c7644ec
Ashutosh Jha <ashutosh06066@gmail.com>
2025-07-27 16:51:24 +0530

    27-07

--- A_Football.cpp
+++ A_Football.cpp
@@ -1,26 +1,28 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define pb push_back
 int main()
 {
     string s;
     cin >> s;
-    int count_1 = 0, count_0 = 0;
     bool ans = false;
+    int count_1 = 0, count_2 = 0;
     for (int i = 0; i < s.length(); i++)
     {
-        if (s[i] == '0')
-        {
-            count_0++;
-            count_1 = 0;
-        }
-        else
+        if (s[i] == '1')
         {
             count_1++;
-            count_0 = 0;
+            count_2 = 0;
         }
-        if (count_0 == 7 || count_1 == 7)
+        else if 
+            (s[i] == '0')
+            {
+                count_2++;
+                count_1 = 0;
+            }
+        if (count_1 == 7 || count_2 == 7)
             ans = true;
     }
-    ans == true ? cout << "YES" : cout << "NO";
+    cout << (ans ? "YES" : "NO");
 }
